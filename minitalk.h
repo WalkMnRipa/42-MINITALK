d6379c18c2386ed2f3c63344237673be785b1dde
Joey Cohen <jcohen@made-f0Br6s3.clusters.42paris.fr>
2024-09-08 17:20:41 +0200

    tst

--- minitalk.h
+++ minitalk.h
@@ -6,14 +6,14 @@
 /*   By: jcohen <jcohen@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/23 15:22:16 by jcohen            #+#    #+#             */
-/*   Updated: 2024/07/06 22:19:06 by jcohen           ###   ########.fr       */
+/*   Updated: 2024/09/08 17:14:27 by jcohen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
-# include "./ft_printf/includes/ft_printf.h"
+# include "./libft/ft_printf.h"
 # include "./libft/libft.h"
 # include <signal.h>
 # include <unistd.h>
